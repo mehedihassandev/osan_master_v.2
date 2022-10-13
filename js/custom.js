97e7e0defc03a90229754360fac8660e7a171cc1
mehedihassandev <mh.web.mehedihassan@gmail.com>
2022-10-13 21:30:15 +0600

    Fully Responsive

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+$(function () {
+
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'UA-23581568-13');
+
+
+
+
+
+
+})();
